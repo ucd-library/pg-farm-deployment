dd08b692b39dc91a400f73f1bf40cc9e17723b3e
Justin Merz <jrmerz@gmail.com>
2024-12-05 11:42:26 -0800

    fixes for; build, local-dev pg tags, vs code tasks

--- cmds/build-local-dev.sh
+++ cmds/build-local-dev.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
 
+if [[ $BUILD_PG == 'true' ]]; then
+  cork-kube build exec \
+    -p postgres \
+    -v 16 \
+    --no-cache-from
+fi
+
 cork-kube build exec \
   -p pg-farm \
-  -v sandbox \
+  -v main \
   -o local-dev \
   --no-cache-from
